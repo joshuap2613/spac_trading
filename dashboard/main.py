4d85d0b69f867b3e87aa4d378361bf18a54db45c
prupes <joshua.prupes@utexas.edu>
2021-03-08 18:17:36 -0600

    updated the framework for what the reddit/twitter website should look like

--- dashboard/main.py
+++ dashboard/main.py
@@ -32,6 +32,7 @@ def get_reddit_sentiment(start, end, ticker):
 
 app.layout = html.Div([
     dcc.Graph(id='sentiment-v-time'),
+    dcc.Graph(id='twitter-sentiment'),
     dcc.Input(id='ticker', value='AAPL', type='text'),
     dcc.Input(id='person1', value='', type='text'),
     dcc.Input(id='person2', value='', type='text'),
@@ -46,7 +47,8 @@ app.layout = html.Div([
         end_date=datetime.now().date()
     ),
     html.Button('Go', id='button'),
-    html.H1('Correlation: ', id='correlation'),  
+    html.H1('Reddit Correlation: ', id='correlation-reddit'),
+    html.H1('Twitter Correlation: ', id='correlation-twitter'),
 ])
 
 @app.callback(
@@ -59,6 +61,7 @@ app.layout = html.Div([
         State(component_id='person4', component_property='value'),
         State(component_id='date-picker', component_property='start_date'),
         State(component_id='date-picker', component_property='end_date')])
+
 def get_plot(n_clicks, ticker, p1, p2, p3, p4, start_date, end_date,):
     print(ticker, start_date, end_date)
 
@@ -87,7 +90,7 @@ def get_plot(n_clicks, ticker, p1, p2, p3, p4, start_date, end_date,):
     return fig
 
 @app.callback(
-    Output('correlation', component_property='children'),
+    Output('correlation-reddit', component_property='children'),
     [Input('button', 'n_clicks')],
     state=[State(component_id='ticker', component_property='value'),
         State(component_id='person1', component_property='value'),
@@ -113,7 +116,75 @@ def get_correlation(n_clicks, ticker, p1, p2, p3, p4, start_date, end_date):
 
     df = correlation(ticker, start, end, score)
 
-    return 'Correlation: ' + str(df['Returns'].corr(df['Score(1)']))
+    return 'Reddit Correlation: ' + str(df['Returns'].corr(df['Score(1)']))
+
+@app.callback(
+    Output('twitter-sentiment', 'figure'),
+    [Input('button', 'n_clicks')],
+    state=[State(component_id='ticker', component_property='value'),
+        State(component_id='person1', component_property='value'),
+        State(component_id='person2', component_property='value'),
+        State(component_id='person3', component_property='value'),
+        State(component_id='person4', component_property='value'),
+        State(component_id='date-picker', component_property='start_date'),
+        State(component_id='date-picker', component_property='end_date')])
+
+def get_twitter_plot(n_clicks, ticker, p1, p2, p3, p4, start_date, end_date,):
+    print(ticker, start_date, end_date)
+
+    start = datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.strptime(end_date, "%Y-%m-%d")
+
+    df = get_reddit_sentiment(start, end, ticker)
+    yf = get_pricing(start, end, ticker)
+
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    fig.add_trace(
+        go.Scatter(x=df['Date'], y=df['Score']),
+        secondary_y=False
+    )
+
+    fig.add_trace(
+        go.Scatter(x=yf['Date'], y=yf['Close']),
+        secondary_y=True
+    )
+
+    fig.update_yaxes(title_text="<b>Sentiment</b> axis", secondary_y=False)
+    fig.update_yaxes(title_text="<b>Close</b> axis", secondary_y=True)
+
+    return fig
+
+@app.callback(
+    Output('correlation-twitter', component_property='children'),
+    [Input('button', 'n_clicks')],
+    state=[State(component_id='ticker', component_property='value'),
+        State(component_id='person1', component_property='value'),
+        State(component_id='person2', component_property='value'),
+        State(component_id='person3', component_property='value'),
+        State(component_id='person4', component_property='value'),
+        State(component_id='date-picker', component_property='start_date'),
+        State(component_id='date-picker', component_property='end_date')])
+def get_twitter_correlation(n_clicks, ticker, p1, p2, p3, p4, start_date, end_date):
+    import time 
+
+    time.sleep(1)
+    start = datetime.strptime(start_date, "%Y-%m-%d")
+    end = datetime.strptime(end_date, "%Y-%m-%d")
+
+    data = get_reddit_data(start, end, ticker, 'wallstreetbets')
+    for k in data:
+        data[k] = datetime.fromtimestamp(data[k])
+        data[k] = data[k].replace(hour=0, minute=0, second=0)
+    
+    # print(data)
+    score = get_scores(data)
+
+    df = correlation(ticker, start, end, score)
+
+    return 'Twitter Correlation: ' + str(df['Returns'].corr(df['Score(1)']))
+
 
 if __name__ == '__main__':
     # TODO: add time ranges
